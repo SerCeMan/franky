3b7a6cd292c1e141816a5db3197330b43c554aef
Sergey Tselovalnikov <Sergey.Tselovalnikov@jetbrains.com>
2016-06-20 13:27:28 +0300

    Plugin structure changed

---
+++ franky-intellij/src/main/java/me/serce/franky/Fdfdf.java
@@ -0,0 +1,15 @@
+package me.serce.franky;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by Sergey.Tselovalnikov on 6/20/16.
+ */
+public class Fdfdf extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
